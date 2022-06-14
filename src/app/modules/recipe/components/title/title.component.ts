64c0fbd6726739eab00624ff0902183b842f69e1
ourainbows <casasjuanfelipe@gmail.com>
2022-06-14 15:27:12 -0500

    Added correct icons in recipe title

--- src/app/modules/recipe/components/title/title.component.ts
+++ src/app/modules/recipe/components/title/title.component.ts
@@ -1,20 +1,41 @@
+import { Recipe } from './../../../../shared/models/recipe.model';
+import { RecipesService } from 'src/app/core/services/recipes.service';
 import { Component, Input, OnInit } from '@angular/core';
+import { ClipboardService } from 'ngx-clipboard';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
-  styleUrls: ['./title.component.scss']
+  styleUrls: ['./title.component.scss'],
 })
 export class TitleComponent implements OnInit {
+  @Input() name = '';
+  @Input() score = 0;
+  @Input() likes: number[] | null = null;
+  @Input() recipe!: Recipe;
 
-  @Input() name= ""
-  @Input() score = 0
-  @Input() likes : number[] | null = null
+  userId = 1; // provisioal userId
 
+  constructor(
+    private recipeService: RecipesService,
+    private clipboardApi: ClipboardService,
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  likeRecipe() {
+    if (this.recipe.likes.includes(this.userId)) {
+      this.recipe.likes.splice(this.recipe.likes.indexOf(this.userId), 1);
+    } else {
+      this.recipe.likes.push(this.userId);
+    }
+    this.recipeService
+      .updateLikesRecipe(this.recipe.id, this.recipe.likes)
+      .subscribe();
   }
 
+  copyText() {
+    this.clipboardApi.copyFromContent(window.location.href);
+    console.log(window.location.href);
+  }
 }
