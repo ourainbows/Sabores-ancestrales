26c3fc0a896b5c8b6ab30aee400067df418b9e13
Daniel Mateo Patarroyo Molina <danielmateopatarroyomolina@gmail.com>
2022-07-27 21:06:59 -0500

    ngOnInit  profile.component.ts and .html fixed

--- src/app/core/services/users/users.service.ts
+++ src/app/core/services/users/users.service.ts
@@ -1,24 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
-import { User } from 'src/app/shared/models/user.model'; 
+import { Observable } from 'rxjs';
+import { User } from 'src/app/shared/models/user.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UsersService {
   private apiUsers = 'http://localhost:3000/user';
-  constructor(private htttp: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
   getUsers(): Observable<User[]> {
-    return this.htttp.get<User[]>(this.apiUsers);
+    return this.http.get<User[]>(this.apiUsers);
   }
 
-  getUserById(id_User: string): Observable<User> {
-    return this.htttp.get<User>(`${this.apiUsers}/${id_User}`).pipe(
-      map((user) => {
-        return user;
-      }),
-    );
+  getUserById(userId: string): Observable<User> {
+    return this.http.get<User>(`${this.apiUsers}/${userId}`)
   }
 }
