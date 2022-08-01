fc5e9936295e02fbbe395640900dfa4474aa8375
ourainbows <casasjuanfelipe@gmail.com>
2022-07-31 22:06:54 -0500

    Created feedback route and its components

--- src/app/modules/admin/modules/feedback/feedback-routing.module.ts
+++ src/app/modules/admin/modules/feedback/feedback-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FeedbackPageComponent } from './page/feedback-page/feedback-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+    path: '', component: FeedbackPageComponent,
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/admin/modules/feedback/feedback.module.ts
+++ src/app/modules/admin/modules/feedback/feedback.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FeedbackRoutingModule } from './feedback-routing.module';
+import { FeedbackPageComponent } from './page/feedback-page/feedback-page.component';
+import { ScoreComponent } from './components/score/score.component';
+import { UserMessagesComponent } from './components/user-messages/user-messages.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FeedbackPageComponent,
+    ScoreComponent,
+    UserMessagesComponent
+  ],
   imports: [
     CommonModule,
     FeedbackRoutingModule
