bbc3daee70aff7fd7833cf0bba3aa5b4ab93b05c
Daniel Mateo Patarroyo Molina <danielmateopatarroyomolina@gmail.com>
2022-08-10 15:43:35 -0500

    Footer.component (initial version)

--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -6,10 +6,12 @@ import { ProfilePageComponent } from './page/profile-page/profile-page.component
 import { StatisticsComponent } from './components//statistics/statistics.component';
 import { UserDescriptionComponent } from './components/user-description/user-description.component';
 import { TabRecipesComponent } from './components/tab-recipes/tab-recipes.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+
 
 
 @NgModule({
   declarations: [ProfilePageComponent, StatisticsComponent, UserDescriptionComponent, TabRecipesComponent],
-  imports: [CommonModule, ProfileRoutingModule],
+  imports: [CommonModule, ProfileRoutingModule, SharedModule],
 })
 export class ProfileModule {}
