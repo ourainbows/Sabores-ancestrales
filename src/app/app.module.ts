556237416ddfbf54eb9bdb84fc5ddb0ca842b880
Daniel Mateo Patarroyo Molina <danielmateopatarroyomolina@gmail.com>
2022-08-15 21:19:19 -0500

    footer fixed and back-button created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { FormsModule } from '@angular/forms';
     BrowserAnimationsModule,
     MatIconModule,
     ClipboardModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
