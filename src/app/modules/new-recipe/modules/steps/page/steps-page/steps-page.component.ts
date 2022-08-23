da0025635fef86aebba92411856567e5e6ddaaad
ourainbows <casasjuanfelipe@gmail.com>
2022-08-22 23:16:09 -0500

    Created functions to upload images to firebase and obtain url of the uploaded image

--- src/app/modules/new-recipe/modules/steps/page/steps-page/steps-page.component.ts
+++ src/app/modules/new-recipe/modules/steps/page/steps-page/steps-page.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { finalize, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-steps-page',
   templateUrl: './steps-page.component.html',
-  styleUrls: ['./steps-page.component.scss']
+  styleUrls: ['./steps-page.component.scss'],
 })
 export class StepsPageComponent implements OnInit {
+  private basePath = '/uploads';
+  constructor(private storage: AngularFireStorage) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  upload(event: any) {
+    const file = event.target.files[0];
+    this.uploadImage(file);
   }
 
+  uploadImage(fileUpload: any) {
+    const filePath = `/images/${fileUpload.name}-${Math.random()}`;
+    const storageRef = this.storage.ref(filePath);
+    const uploadTask = this.storage.upload(filePath, fileUpload);
+    uploadTask
+      .snapshotChanges()
+      .pipe(
+        finalize(() => {
+          storageRef.getDownloadURL().subscribe((downloadURL) => {
+            console.log( downloadURL);
+          });
+        })
+      )
+      .subscribe();
+  }
+
+  ngOnInit(): void {}
 }
--- src/app/modules/new-recipe/modules/steps/steps.module.ts
+++ src/app/modules/new-recipe/modules/steps/steps.module.ts
@@ -4,6 +4,11 @@ import { CommonModule } from '@angular/common';
 import { StepsRoutingModule } from './steps-routing.module';
 import { StepsPageComponent } from './page/steps-page/steps-page.component';
 
+import { AngularFireModule } from '@angular/fire/compat';
+// import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+// import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 
 @NgModule({
   declarations: [
@@ -11,7 +16,17 @@ import { StepsPageComponent } from './page/steps-page/steps-page.component';
   ],
   imports: [
     CommonModule,
-    StepsRoutingModule
+    StepsRoutingModule,
+    AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyDN5pIymvx7y8BmJWH456oohyqqlCXOlKs',
+      authDomain: 'saboresancestrales-56030.firebaseapp.com',
+      projectId: 'saboresancestrales-56030',
+      storageBucket: 'saboresancestrales-56030.appspot.com',
+      messagingSenderId: '240351398364',
+      appId: '1:240351398364:web:11f63e50e93ec5387ea719',
+    }),
+    AngularFireDatabaseModule,
+    AngularFireStorageModule,
   ]
 })
 export class StepsModule { }
