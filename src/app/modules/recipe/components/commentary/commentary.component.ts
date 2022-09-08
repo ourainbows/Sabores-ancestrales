1f985fda4b2e5b07b187440e98c6eddb48d97b51
ourainbows <casasjuanfelipe@gmail.com>
2022-09-07 23:27:46 -0500

    Feature, option to upload images when user comment

--- src/app/modules/recipe/components/commentary/commentary.component.ts
+++ src/app/modules/recipe/components/commentary/commentary.component.ts
@@ -1,5 +1,7 @@
 import { Commentary } from './../../../../shared/models/recipe.model';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { last, map, mergeMap, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-commentary',
@@ -7,27 +9,47 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./commentary.component.scss'],
 })
 export class CommentaryComponent implements OnInit {
-
   today = new Date();
-  
+
   newComment: Commentary = {
     user: 'Chef Teste',
     comment: '',
     photoUser:
       'https://images.unsplash.com/photo-1581299894007-aaa50297cf16?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Y2hlZnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
     photoRecipe: '',
-    date: (`${this.today.getFullYear()}-${this.today.getMonth()}-${this.today.getDate()}`),
+    date: `${this.today.getFullYear()}-${this.today.getMonth()}-${this.today.getDate()}`,
     likes: [],
     userId: 1, // temporary
   };
+  imageSrc: any = '';
 
   @Output() newCommentEvent = new EventEmitter<Commentary>();
 
   createNewComment() {
-    this.newCommentEvent.emit(this.newComment);
+    this.uploadImage(this.imageSrc).subscribe((url) => {
+      this.newCommentEvent.emit({...this.newComment, photoRecipe: url});
+    });
   }
 
-  constructor() {}
+  readURL(event: any): void {
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+      this.imageSrc = file;
+    }
+  }
+  uploadImage(fileUpload: any) {
+    const filePath = `/images/${fileUpload.name}-${Math.random()}`;
+    const storageRef = this.storage.ref(filePath);
+    const uploadTask = this.storage.upload(filePath, fileUpload);
+
+    return <Observable<string>>uploadTask.snapshotChanges().pipe(
+      last(),
+      mergeMap(() => {
+        return storageRef.getDownloadURL().pipe(map((url) => url));
+      })
+    );
+  }
+  constructor(private storage: AngularFireStorage) {}
 
   ngOnInit(): void {}
 }
