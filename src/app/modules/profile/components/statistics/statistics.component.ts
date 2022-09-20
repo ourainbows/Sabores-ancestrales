be4781bd0f3a0ce3adb90f21652f074815b38327
ourainbows <casasjuanfelipe@gmail.com>
2022-09-19 19:53:53 -0500

    Feature, new recipe button redirects to login if user isn't login

--- src/app/modules/profile/components/statistics/statistics.component.ts
+++ src/app/modules/profile/components/statistics/statistics.component.ts
@@ -4,13 +4,11 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
-  styleUrls: ['./statistics.component.scss']
+  styleUrls: ['./statistics.component.scss'],
 })
 export class StatisticsComponent implements OnInit {
   @Input() user!: User;
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
