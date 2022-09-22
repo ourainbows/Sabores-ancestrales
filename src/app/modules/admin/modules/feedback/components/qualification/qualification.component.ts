4b612c90a997a0b5b5d79f2cc5bb468f90f17281
ourainbows <casasjuanfelipe@gmail.com>
2022-09-22 18:55:18 -0500

    Feature, render feedback and feedback data

--- src/app/modules/admin/modules/feedback/components/qualification/qualification.component.ts
+++ src/app/modules/admin/modules/feedback/components/qualification/qualification.component.ts
@@ -3,23 +3,20 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-qualification',
   templateUrl: './qualification.component.html',
-  styleUrls: ['./qualification.component.scss']
+  styleUrls: ['./qualification.component.scss'],
 })
 export class QualificationComponent implements OnInit {
-
   @Input() score = {
-    "qualification": 4.5,
-    "quantity": 2115,
-    "five-star": 1985,
-    "four-star": 353,
-    "three-star": 130,
-    "two-star": 90,
-    "one-star": 33
-  }
+    qualification: 0,
+    quantity: 0,
+    oneStar: 0,
+    twoStar: 0,
+    threeStar: 0,
+    fourStar: 0,
+    fiveStar: 0,
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/modules/admin/modules/feedback/page/feedback-page/feedback-page.component.ts
+++ src/app/modules/admin/modules/feedback/page/feedback-page/feedback-page.component.ts
@@ -13,9 +13,23 @@ export class FeedbackPageComponent implements OnInit {
 
   messages : any = []
 
+  stats : any = {
+    qualification: 0,
+    quantity: 0,
+    oneStar: 0,
+    twoStar: 0,
+    threeStar: 0,
+    fourStar: 0,
+    fiveStar: 0,
+  }
+
   ngOnInit(): void {
     this.feedbackService.getFeedback().subscribe((data: Feedback[]) => {
       this.messages = data
+    })
+    this.feedbackService.getFeedbackStats().subscribe((data: any) => {
+      this.stats = data
+      console.log(data)
     })
   }
 
