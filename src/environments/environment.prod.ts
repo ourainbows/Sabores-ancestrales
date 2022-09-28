d9701361666cfcaef7dae7c5af9635a6ad05b2d0
ourainbows <casasjuanfelipe@gmail.com>
2022-09-27 22:49:18 -0500

    Add environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  api: 'https://saboresancestrales.onrender.com',
+  firebaseConfig: {
+    apiKey: 'AIzaSyDN5pIymvx7y8BmJWH456oohyqqlCXOlKs',
+    authDomain: 'saboresancestrales-56030.firebaseapp.com',
+    projectId: 'saboresancestrales-56030',
+    storageBucket: 'saboresancestrales-56030.appspot.com',
+    messagingSenderId: '240351398364',
+    appId: '1:240351398364:web:11f63e50e93ec5387ea719',
+  },
 };
